2385b42c3ba6a9867bf7624b7cb27cdc1c0c232c
Pablo Dominguez <Domi@usal.es>
2024-06-03 10:41:01 +0200

    muchos cambios otra vez xd

--- modules/tabletMediapipe/android/build/generated/data_binding_trigger/debug/com/example/tablet_mediapipe/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.tablet_mediapipe;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- modules/tabletMediapipe/android/build/generated/data_binding_trigger/debugAndroidTest/com/example/tablet_mediapipe/test/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.tablet_mediapipe.test;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
