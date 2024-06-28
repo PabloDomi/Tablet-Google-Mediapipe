30ea7c20d902a99dc6de34871f359b6ca7cd9047
Pablo Dominguez <Domi@usal.es>
2024-06-28 13:04:28 +0200

    expo-build-properties

--- modules/tabletMediapipe/android/src/main/java/com/example/tablet_mediapipe/tablet_mediapipeView.kt
+++ modules/tabletMediapipe/android/src/main/java/com/example/tablet_mediapipe/tablet_mediapipeView.kt
@@ -1,15 +1,17 @@
 package com.example.tablet_mediapipe
 
-
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
+import com.example.tablet_mediapipe.databinding.ActivityMainBinding
 import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.ExpoModulesHelper
+
 import expo.modules.kotlin.views.ExpoView
 
 class tablet_mediapipeView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
     init {
-        // Inflate el layout de fragment_camera directly
-        val inflater = LayoutInflater.from(context)
-        inflater.inflate(R.layout.activity_main, this, true)
+        val intent = Intent(context, MainActivity::class.java)
+        context.startActivity(intent)
     }
 }
